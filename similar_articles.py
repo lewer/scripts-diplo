62602232332dc5c0504dae851d71cbdcfd33e2a5
Gabriel Lewertowski <gabriel@redclockwork>
2014-06-17 18:29:33 +0200

    sys.stdin.read() in similar_articles

--- similar_articles.py
+++ similar_articles.py
@@ -17,17 +17,17 @@ def find_similar_articles(corpus_name, method, id=None, content=None):
     corpus_file = corpus_name + '_' + method + '.mm'
     index_file = corpus_name + '_' + method + '_index'
     docid_file = corpus_name + '_docid.txt'
-    
+
     try:
         corpus = corpora.mmcorpus.MmCorpus(corpus_file)
     except Exception:
         raise IOError('Impossible de charger le fichier %s' % (corpus_file))
-        
+
     try:
         index = similarities.docsim.Similarity.load(index_file)
     except Exception:
         raise IOError('Impossible de charger le fichier %s' % (index_file))
-        
+
     if id is not None:  
         corpus_id = utils.get_article_by_id(id, docid_file)
         tokens = corpus[corpus_id]
@@ -39,24 +39,24 @@ def find_similar_articles(corpus_name, method, id=None, content=None):
             id2word = corpora.dictionary.Dictionary.load_from_text(dico_file)
         except Exception:
             raise IOError("Impossible de charger le fichier %s" % (dico_file))
-            
+
         if method == 'tfidf':
             model_file = corpus_name + '_tfidf_model'
             model = models.tfidfmodel.TfidfModel.load(model_file)
-            
+
         elif method.startswith('lsi'):
             model_file = corpus_name + '_' + args.method + '_model'
             model = models.lsimodel.LsiModel.load(model_file)
-            
+
         elif method.startswith('lda'):
             model_file = corpus_name + '_' + args.method + '_model'
             model = models.ldamodel.LdaModel.load(model_file)
-        
+
         tokens = model[id2word.doc2bow(utils.tokenize(content))]
-    
+
     else:
         raise Exception("Il faut fournir un id ou un contenu")
-    
+
     sims = index[tokens]   
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
 
@@ -68,18 +68,16 @@ if __name__ == '__main__':
     parser.add_argument('corpus_name', type=str, help='Le nom du corpus')
     parser.add_argument('method', type=str, help="La méthode utilisée (lda, lsi, tfidf)")
     parser.add_argument('--id', type=int, help="L'id de l'article")
-    parser.add_argument('--file', type=str, help="Un fichier contenant du texte")
     parser.add_argument('-v', '--verbose', action='store_true',
-                        help="Afficher les messages d'information")
+            help="Afficher les messages d'information")
     args = parser.parse_args()
 
     if args.verbose:
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    
+
     if args.id:
         print find_similar_articles(args.corpus_name, args.method, id=args.id)
     else:
-        with open(args.file) as f:
-            content = unicode(f.read(), 'utf8')
-            print find_similar_articles(args.corpus_name, args.method, content=content)
-
+        content = unicode(sys.stdin.read(), 'utf8')
+        print content
+        print find_similar_articles(args.corpus_name, args.method, content=content)
